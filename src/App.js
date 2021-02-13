dafbbc7dbc6e560683937219948366c67f3746e5
Enzo <enzosantamaria@outlook.fr>
2021-02-13 21:49:23 +0100

    2. Navigation, BrowserRouter etc...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import  { Project1, Project2, Project3, Project4 } from "./pages/Projects";
+
 
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hellp</h1>
-    </div>
+    <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/project-1' component={Project1}/>
+      <Route exact path='/project-2' component={Project2}/>
+      <Route exact path='/project-3' component={Project3}/>
+      <Route exact path='/project-4' component={Project4}/>
+      <Route exact path='/Contact' component={Contact}/>
+      <Redirect to='/' />
+    </Switch>
+    </BrowserRouter>
   );
 }
 
